4493f94e4c67c7a9548b69cd603bc93ab022ce60
marcusDevGit <marcus_psc@hotmail.com>
2026-02-05 14:51:01 -0300

    chore: ignore dist folders and remove them from tracking

--- expens-tracker-api/dist/config/jwt.js
+++
@@ -1,4 +0,0 @@
-import "dotenv/config";
-import { env } from "../config/env.js";
-export const jwtConfig = { secret: env.jwtSecret, expiresIn: "1d" };
-export const jwtRefreshToken = { secret: env.jwtRefreshSecret, expiresIn: "7d" };
